609a32fe46b99500eeff75936a027adc92f31b48
TomBoughton <72128179+TomBoughton@users.noreply.github.com>
2021-03-18 12:06:21 +0000

    day 1

---
+++ Die Trying/Assets/Scripts/StaticClass.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SharedData
+{
+    public static int score = 10;
+    public static int health = 100;
+}
