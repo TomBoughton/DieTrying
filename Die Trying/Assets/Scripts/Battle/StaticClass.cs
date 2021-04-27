9dffa1028c283724621b44930a3ae659d54e77a9
TomBoughton <72128179+TomBoughton@users.noreply.github.com>
2021-04-27 18:27:42 +0100

    27/04

---
+++ Die Trying/Assets/Scripts/Battle/StaticClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SharedData
+{
+    public static int mp = 10;
+    public static int health = 1;
+    public static int defence = 1;
+    public static int corruption = 0;
+    public static int tolerance = 1;
+    public static int attack = 1;
+    public static int exp = 0;
+    public static int level = 1;
+}
