fa5f7d3dab5fddd5d9c32e0bc946b3103cd4e494
TomBoughton <72128179+TomBoughton@users.noreply.github.com>
2021-05-05 12:07:00 +0100

    12/06

--- Die Trying/Assets/Scripts/Game/BookPuzzle.cs
+++ Die Trying/Assets/Scripts/Game/BookPuzzle.cs
@@ -8,4 +8,57 @@ public class BookPuzzle : MonoBehaviour
 {
 
     public Text TextBox;
+    private string[] books = {"","","","",""};
+    private int x = 0;
+    
+
+    void Start()
+    {
+        TextBox.text =("There are five books here. If they are placed in the correct order, You will get the key you need.");
+    }
+
+    void Update()
+    {
+        if(x==5)
+        {
+            string y = string.Join(", ", books);
+            TextBox.text = (y);
+            x = 0;
+        }
+    }
+
+    public void BlueClicked()
+    {
+        books[x] = "Blue";
+        TextBox.text =(books[x]);
+        x++;
+        
+    }
+
+    public void GreenClicked()
+    {
+        books[x] = "Green";
+        TextBox.text =(books[x]);
+        x++;
+    }
+
+    public void RedClicked()
+    {
+        books[x] = "Red";
+        TextBox.text =(books[x]);
+        x++;
+    }
+
+    public void YellowClicked()
+    {
+        books[x] = "Yellow";
+        TextBox.text =(books[x]);
+        x++;
+    }
+    public void PurpleClicked()
+    {
+        books[x] = "Purple";
+        TextBox.text =(books[x]);
+        x++;
+    }
 }
